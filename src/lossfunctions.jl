65378ec9d914208570e01d566e36015d6c403501
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2021-03-17 23:21:25 +0530

    Add rastrigin and rosenbrock loss functions

--- src/lossfunctions.jl
+++ src/lossfunctions.jl
@@ -1,3 +1,21 @@
 """
 This file contains a vareity of loss functions that we use to check our optimizers on.
 """
+
+"""
+
+"""
+function rastrigrin(A::Real, x::Vector{T}) where {T<:Real}
+    n = size(x, 1)
+    return A * n + sum(x.^ 2 .- cos.(x))
+end
+
+
+function rosenbrock(x::Vector{T}) where {T<:Real}
+    n = size(x, 1)
+    f = 0.0
+    for i = 1:n-1
+        f += 100 * (x[i+1] - x[i])^2 + (1 - x[i])^2
+    end
+    return f
+end
